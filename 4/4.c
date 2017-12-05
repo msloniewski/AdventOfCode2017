a6eee0f451ef25a1b803f319e661ffe490b8f97b
sloniewskim <marcin.sloniewski@gmail.com>
2017-12-05 22:28:37 +0100

    4 day part 1 done.

---
+++ 4/4.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_NUM_WORDS 20
+int main()
+{
+	FILE *file = fopen("input.txt", "r");
+	char line[256];
+	
+	char *words[MAX_NUM_WORDS];
+	char *currentWord = NULL;
+	int noOfWords = 0;
+	int i=0;
+	int j=0;
+	int Passphrases = 0;
+	int flag = 0;
+	int all =0;
+	for (i=0; i< MAX_NUM_WORDS; i++)
+	{
+		words[i] = malloc(sizeof(char)*90);
+	}
+
+	while (fgets(line, sizeof(line), file))
+	{
+
+		noOfWords = 1;
+
+		currentWord = strtok(line," ");
+		while (currentWord != NULL  )
+		{
+			strcpy(words[noOfWords],currentWord);
+			noOfWords++;
+			currentWord = strtok(NULL," ");
+		}
+		//strcpy(words[noOfWords],line);
+
+		int len = strlen(words[noOfWords-1]);
+		if ( words[noOfWords-1][len-1] == 10 ) //Line feed
+		{
+			words[noOfWords-1][len-1] =0;
+		}
+
+		flag = 0;
+		for (i=1; i< noOfWords-1 ; i++)
+		{
+			for (j=i+1; j< noOfWords; j++)
+			{
+				if ( !strcmp( words[i], words[j]) )
+				{
+					flag = 1;
+				}
+				
+			}
+
+		}
+		if ( !flag)  
+			Passphrases++;
+		all++;
+		
+	}
+
+	printf("ans %d\n",Passphrases);
+	printf("all %d\n",all);
+
+	fclose(file);
+	while (i=0, i< MAX_NUM_WORDS, i++)
+	{
+		free (words[i]);
+	}
+
+}
