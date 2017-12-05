93d464d7322016aec5fb43ef8f9282cf60277572
sloniewskim <marcin.sloniewski@gmail.com>
2017-12-05 21:00:27 +0100

    second puzzle of day 3 done.

--- 3/3_2.lua
+++ 3/3_2.lua
@@ -1,67 +1,72 @@
 #!/usr/bin/lua5.1
 
+
+function add(x,y)
+	sum=0
+	for i_l = -1,1,1 do
+		for j_l = -1,1 do
+			if ( mat[x+i_l][y+j_l] ~= nil ) then
+				in_x = x+i_l
+				in_y= y+j_l
+				sum = sum + mat[in_x][in_y]
+			end
+		end
+	end
+	mat[x][y]=sum
+	last_val =sum
+	io.write( x, " ", y, " " , sum,"\n")
+end
+
 threshold = 265149
-n = 0
-val = 1
+--threshold = 2
+n = 2
 
-while (val < threshold)
-then 
-	rad = 0
-	while ( ( 2 * rad + 1  )^2 < n)
-	do
-		rad = rad +1
-	end
-	side = 2 * rad 
-	offset = n - ( rad * 2 - 1 )^2 - 1
-	offset2 = offset % side  
+side = 2
+
+last_val=1 
 	
-	side_num = floor (offset / side)
+mat={}
+for i=-1000,1000 do
+	mat[i] = {}
+end
 
-	if side_num == 0 
-		then asd
-	elseif	side_num == 1
-		then asdasd
-	elseif side_num == 2
-		then asdasd
-	elseif  side_num == 3
-		then asdasd
+curr_x = 0
+curr_y = 0
+mat[0][0] = 1
+
+while (last_val < threshold) do
+	curr_x=curr_x + 1
+	curr_y=curr_y - 1
+
+	mult_x = -1
+	mult_y = 1
+	for h=1,2 do
+
+			for k=1,side do
+				curr_y = curr_y + mult_y 
+				add(curr_x,curr_y)
+				n=n+1
+				if last_val > threshold then break end
+			end
+			if last_val > threshold then break end
+
+
+			for k=1,side do
+				curr_x =curr_x + mult_x  
+				add(curr_x,curr_y)
+				n=n+1
+				if last_val > threshold then break end
+			end
+			if last_val > threshold then break end
+
+		mult_x = 1
+		mult_y = -1
+		if last_val > threshold then break end
 	end
-	
-	if offset == 0 
-	then
-		asdasd
-	end 
-	if offset == side -1
-	then 
-		asdasdasd
-	end
-	
-	if offset == side -2 
-	then 
-		asdasdasdasd
-	end
 
-
-	else 
-
-
---	val = tab[n-1]
-	
-	n += 1 --lets gooo
+	side =side+2
 end
 
 
-io.write( n  , "n\n")
-io.write( side  , "side\n")
-io.write( offset , "offset\n")
-io.write( offset2 , "offset2\n")
-io.write( distance , "distance\n")
-
---      36  35  34  33  32  
--- 17   16  15  14  13  30 
--- 18    5   4   3  12  29
--- 19    6   1   2  11  28
--- 20    7   8   9  10  27
--- 21   22  23  24  25  26
---
---
+io.write( n, "index\n")
+io.write( last_val, "val bigger\n")
