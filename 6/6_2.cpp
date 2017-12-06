98be2e3e9577215b9469b8f0ab66f5cf41bb1f71
sloniewskim <marcin.sloniewski@gmail.com>
2017-12-06 21:10:28 +0100

    day 6 part 2 done

---
+++ 6/6_2.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std; 
+//0 if the same 1 if different
+int compare (const vector<int> &a, const vector<int> &b)
+{
+	for (int i=0; i < a.size(); i++)
+	{
+		if (a[i] != b[i])
+			return 1;
+	}
+	return 0;
+}
+
+int findBiggest (const vector<int> &a)
+{
+	int comp =0;
+	int index =0;
+	for (int i=0; i< a.size(); i++)
+	{
+		if (a[i]> comp)
+		{
+			comp = a[i];
+			index =i;
+		}
+	}
+	return index;
+}
+int haveISeenItBefore(const vector<int> &a, const vector<vector<int>> &b)
+{
+	for (int i=0 ; i< b.size();i++)
+	{
+		if ( !compare(a,b[i]) )
+			return i+1;
+	}
+	return 0;
+}
+int main()
+{
+	ifstream input("input.txt");
+
+	vector<int> current {10,3,15,10,5,15,5,15,9,2,5,8,5,2,3,6};
+	vector<vector<int>> previous;
+
+	
+	int no = 0;
+	while ( !haveISeenItBefore(current, previous) )
+	{	
+		previous.push_back(current);
+		int index = findBiggest( current );
+		int toDo = current[index];
+		current[index] = 0;
+		while(toDo > 0)
+		{
+			index = (index +1)% current.size();
+			current[index] ++;
+			toDo--;
+		}
+		no++;
+	}
+	cout<<"numb:"<<previous.size() - (haveISeenItBefore(current, previous)-1)<<endl;
+	
+	
+}
